cffaef1d3ab91dd11a1ecb2e2b3e9a4f58fe7c26
liangbo <236058013@qq.com>
2021-07-16 21:53:00 +0800

    String对比

--- common/src/main/java/pers/qxllb/common/test/comparator/ComparatorTest.java
+++ common/src/main/java/pers/qxllb/common/test/comparator/ComparatorTest.java
@@ -3,6 +3,7 @@ package pers.qxllb.common.test.comparator;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Comparator;
 import java.util.List;
@@ -18,8 +19,7 @@ import java.util.stream.Collectors;
  */
 public class ComparatorTest {
 
-    public static void main(String[] args){
-
+    public static void sortting1(){
         List<Student> list = Lists.newArrayList();
 
         Student student = new Student();
@@ -54,13 +54,13 @@ public class ComparatorTest {
         student3.setFans(20L);
         list.add(student3);
 
-       System.out.println("before sort :"+JSON.toJSONString(list));
+        System.out.println("before sort :"+JSON.toJSONString(list));
 
-       //没条件过滤情况，直接原集合多重条件排序,a>b>c
-       list.sort(Comparator.comparing(Student::getBuyCnt,Comparator.reverseOrder())
-                    .thenComparing(Student::getFans,Comparator.reverseOrder())
-                    .thenComparing(Student::getComments,Comparator.reverseOrder())
-                );
+        //没条件过滤情况，直接原集合多重条件排序,a>b>c
+        list.sort(Comparator.comparing(Student::getBuyCnt,Comparator.reverseOrder())
+                .thenComparing(Student::getFans,Comparator.reverseOrder())
+                .thenComparing(Student::getComments,Comparator.reverseOrder())
+        );
         System.out.println("after sort :"+JSON.toJSONString(list));
 
         //根据条件过滤，多重条件排序，A>B>C
@@ -73,12 +73,60 @@ public class ComparatorTest {
             }
             return t;
         }).sorted(  //Comparator.comparing默认是升序排序，用reversed改成降序
-                 Comparator.comparing(Student::getBuyCnt,Comparator.reverseOrder())
-                .thenComparing(Student::getFans,Comparator.reverseOrder())
-                .thenComparing(Student::getComments,Comparator.reverseOrder()))
+                Comparator.comparing(Student::getBuyCnt,Comparator.reverseOrder())
+                        .thenComparing(Student::getFans,Comparator.reverseOrder())
+                        .thenComparing(Student::getComments,Comparator.reverseOrder()))
                 .collect(Collectors.toList());
 
-       System.out.println(JSON.toJSONString(finallyList1));
+        System.out.println(JSON.toJSONString(finallyList1));
+    }
+
+    public static void sortting2(){
+
+        Student stu1 = new Student();
+        stu1.setRoleName("主持人");
+
+        Student stu2 = new Student();
+        stu2.setRoleName("主持人");
+        stu2.setWealthRank(1);
+        stu2.setUpdatetime(123L);
+
+        Student stu3 = new Student();
+        stu3.setRoleName("嘉宾");
+        stu3.setWealthRank(1);
+        stu3.setUpdatetime(300L);
+
+        Student stu4 = new Student();
+        stu4.setWealthRank(5);
+
+        Student stu5 = new Student();
+        stu5.setWealthRank(3);
+
+        Student stu6 = new Student();
+
+        List<Student> list = Lists.newArrayList();
+        list.add(stu1);
+        list.add(stu2);
+        list.add(stu3);
+        list.add(stu4);
+        list.add(stu5);
+        list.add(stu6);
+
+        List<Student> list1 = list.stream().filter(t-> StringUtils.isNotBlank(t.getRoleName()))
+                .sorted(
+                Comparator.comparing(Student::getWealthRank)
+                .thenComparing(Student::getUpdatetime)
+        ).collect(Collectors.toList());
+
+        System.out.println(JSON.toJSONString(list1));
+
+    }
+
+    public static void main(String[] args){
+
+        //sortting1();
+        sortting2();
+
 
     }
 
@@ -96,5 +144,11 @@ class Student{
     private Long fans;
 
     private Long comments;
+
+    private String roleName;
+
+    private Integer wealthRank=-1;
+
+    private Long updatetime;
 
 }
--- common/src/main/java/pers/qxllb/common/test/json/FastJsonTest.java
+++ common/src/main/java/pers/qxllb/common/test/json/FastJsonTest.java
@@ -1,5 +1,11 @@
 package pers.qxllb.common.test.json;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 类描述:
  *
@@ -20,6 +26,23 @@ public class FastJsonTest {
 
 
     public static void main(String[] args){
+
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("roomId", "123456");
+
+        JSONObject body = new JSONObject();
+        body.put("event", "roomHotIncrNotify");
+        body.put("body", JSON.toJSONString(map));
+
+        String str = JSON.toJSONString(body);
+
+
+        JSONObject bodyRecv = JSON.parseObject(str);
+        String event = body.getString("event");
+        JSONObject roomIdJson = JSON.parseObject(body.getString("body"));
+        String roomId = roomIdJson.getString("roomId");
+        System.out.println(roomId);
 
     }
 
